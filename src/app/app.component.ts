13f249dcaa1ebb774be9e8939731c0bb349c113f
nyizeya <nyizeya103@gmail.com>
2023-04-13 10:54:31 +0630

    added typing game exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'the-modern-angular-bootcamp';
+  randomText = faker.lorem.sentence();
+  enteredText = '';
+
+  onInput(event: Event) {
+    this.enteredText = (event.target as HTMLInputElement).value;
+  }
+
+  compare(letter: string, enteredLetter: string): string {
+    if (!enteredLetter) {
+      return 'pending';
+    }
+
+    return enteredLetter === letter ? 'correct' : 'incorrect';
+  }
 }
